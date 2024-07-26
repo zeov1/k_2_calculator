f37776d55ec487c3ba094c544ed87a50787f6681
zeovl <t56k.ezpf@gmail.com>
2024-07-26 23:05:13 +0700

    Shitty calculator (no CSS)

--- calc/views.py
+++ calc/views.py
@@ -2,4 +2,51 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "calc.html")
+    try:
+        a = request.GET['a']
+        a = int(a)
+        a_ok = True
+    except ValueError:
+        a = None
+        a_ok = False
+
+    try:
+        b = request.GET['b']
+        b = int(b)
+        b_ok = True
+    except ValueError:
+        b = None
+        b_ok = False
+
+    try:
+        op = request.GET['op']
+        op_ok = True
+    except ValueError:
+        op = None
+        op_ok = False
+
+    if op == 'plus':
+        op = '+'
+        res = a + b
+    elif op == 'minus':
+        op = '-'
+        res = a - b
+    elif op == 'mult':
+        op = '*'
+        res = a * b
+    elif op == 'divide':
+        op = '/'
+        res = a / b
+    else:
+        res = None
+        op_ok = False
+
+    return render(request, "calc.html", {
+        'a_ok': a_ok,
+        'b_ok': b_ok,
+        'op_ok': op_ok,
+        'a': a,
+        'b': b,
+        'op': op,
+        'res': res,
+    })
--- k_2_calculator/urls.py
+++ k_2_calculator/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from calc.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('calc/', calc.views.index),
+    path('calc/', index),
 ]
