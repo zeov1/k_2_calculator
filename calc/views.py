19dc498d44b91518571e8ce7d327185c94e50c52
zeovl <t56k.ezpf@gmail.com>
2024-07-29 23:13:59 +0700

    it finally works!

--- calc/views.py
+++ calc/views.py
@@ -1,11 +1,57 @@
-from django.http import QueryDict
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect
+
+from .forms import CalcForm
 
 
+@csrf_protect
 def calc(request):
     if request.method == 'POST':
-        print('There is a POST-request')
-        print(request.POST)
+        data = CalcForm(request.POST)
+        ok = True
+        msg = []
+        if data.is_valid():
+            a = data.cleaned_data['a']
+            try:
+                a = float(a)
+            except ValueError:
+                ok = False
+                msg.append('Value a must be float')
+            b = data.cleaned_data['b']
+            try:
+                b = float(b)
+            except ValueError:
+                ok = False
+                msg.append('Value b must be float')
+            op = data.cleaned_data['op']
+            if op == '+':
+                res = a + b if ok else None
+            elif op == '-':
+                res = a - b if ok else None
+            elif op == '*':
+                res = a * b if ok else None
+            elif op == '/':
+                if b != 0:
+                    res = a / b if ok else None
+                else:
+                    res = float('inf') * a if ok else None
+            else:
+                ok = False
+                res = None
+                msg.append(f'Unknown operation: {op}')
+        else:
+            a = b = res = None
+
+        print(res)
+
+        return render(request, 'calc.html', {
+            'ok': ok,
+            'msg': msg,
+            'a': a,
+            'b': b,
+            'res': res,
+        })
+
     return render(request, 'calc.html')
 
 
--- k_2_calculator/settings.py
+++ k_2_calculator/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -25,8 +24,7 @@ SECRET_KEY = 'django-insecure-fvd(c_5_w6jg)s@ptb$21nb($8@d31(h_l%2g%+gjqz4*=jv_6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.169', 'localhost']
-
+ALLOWED_HOSTS = ['192.168.0.169', 'localhost', 'hopelessly-thankful-feline.ngrok-free.app']
 
 # Application definition
 
@@ -70,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'k_2_calculator.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -80,7 +77,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -100,7 +96,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -112,7 +107,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
@@ -123,3 +117,5 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://hopelessly-thankful-feline.ngrok-free.app']
